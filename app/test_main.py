e21642ebecb944ea56511a8e1b8f4e2fd914b574
Artem <artemradckevich@gmail.com>
2026-04-13 18:14:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,26 @@
 from datetime import date
-from unittest import mock
+from unittest.mock import patch
+import app.main as main
 
-from app.main import outdated_products
 
-
-def test_no_outdated_products_returns_empty_list() -> None:
+def test_some_case() -> None:
     products = [
-        {"name": "salmon", "expiration_date": date(2022, 2, 10), "price": 600},
-        {"name": "chicken", "expiration_date": date(2022, 2, 5), "price": 120},
         {"name": "duck", "expiration_date": date(2022, 2, 1), "price": 160},
+        {"name": "chicken", "expiration_date": date(2022, 2, 5), "price": 120},
     ]
 
-    with mock.patch("app.main.datetime.date") as mock_date:
-        mock_date.today.return_value = date(2022, 2, 1)
-        mock_date.side_effect = lambda *a, **kw: date(*a, **kw)
-        assert outdated_products(products) == []
+    with patch("app.main.datetime.date") as mock_date:
+        mock_date.today.return_value = date(2022, 2, 2)
+
+        assert main.outdated_products(products) == ["duck"]
 
 
-def test_outdated_products_returns_products() -> None:
+def test_expiration_date_equal_today_not_outdated() -> None:
     products = [
-        {"name": "salmon", "expiration_date": date(2022, 2, 10), "price": 600},
-        {
-            "name": "chicken",
-            "expiration_date": date(2022, 1, 30),
-            "price": 120},
-        {"name": "duck", "expiration_date": date(2022, 1, 25), "price": 160},
+        {"name": "milk", "expiration_date": date(2022, 2, 2), "price": 50},
     ]
 
-    with mock.patch("app.main.datetime.date") as mock_date:
-        mock_date.today.return_value = date(2022, 2, 1)
-        mock_date.side_effect = lambda *a, **kw: date(*a, **kw)
-        assert outdated_products(products) == ["chicken", "duck"]
+    with patch("app.main.datetime.date") as mock_date:
+        mock_date.today.return_value = date(2022, 2, 2)
+
+        assert main.outdated_products(products) == []
