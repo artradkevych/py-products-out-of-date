0e89839c90ca029b289afed5e8c21582fba15bad
Artem <artemradckevich@gmail.com>
2026-04-13 18:03:53 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,43 +1,30 @@
-import pytest
-from unittest import mock
-from app.main import outdated_products
 from datetime import date
+from unittest import mock
+
+from app.main import outdated_products
 
 
-@pytest.fixture
-def products_template() -> list[dict]:
-    return [
-        {
-            "name": "salmon",
-            "expiration_date": date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": date(2022, 2, 1),
-            "price": 160
-        }
+def test_no_outdated_products_returns_empty_list() -> None:
+    products = [
+        {"name": "salmon", "expiration_date": date(2022, 2, 10), "price": 600},
+        {"name": "chicken", "expiration_date": date(2022, 2, 5), "price": 120},
+        {"name": "duck", "expiration_date": date(2022, 2, 1), "price": 160},
     ]
 
-
-def test_no_outdated_products_returns_empty_list(
-        products_template: list[dict]
-) -> None:
     with mock.patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = date(2022, 2, 1)
         mock_date.side_effect = lambda *a, **kw: date(*a, **kw)
-        assert outdated_products(products_template) == []
+        assert outdated_products(products) == []
 
 
-def test_outdated_products_returns_products(
-        products_template: list[dict]
-) -> None:
+def test_outdated_products_returns_products() -> None:
+    products = [
+        {"name": "salmon", "expiration_date": date(2022, 2, 10), "price": 600},
+        {"name": "chicken", "expiration_date": date(2022, 1, 30), "price": 120},
+        {"name": "duck", "expiration_date": date(2022, 1, 25), "price": 160},
+    ]
+
     with mock.patch("app.main.datetime.date") as mock_date:
-        mock_date.today.return_value = date(2022, 2, 10)
+        mock_date.today.return_value = date(2022, 2, 1)
         mock_date.side_effect = lambda *a, **kw: date(*a, **kw)
-        assert outdated_products(products_template) == ["chicken", "duck"]
+        assert outdated_products(products) == ["chicken", "duck"]
